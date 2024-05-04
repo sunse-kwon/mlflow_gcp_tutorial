6927caa19aefa64fc98bf23cdc1c296f700ffe4b
Sunse Kwon <sunse523@gmail.com>
2024-05-04 23:12:37 +0900

    change: env variable

--- entry-point.sh
+++ entry-point.sh
@@ -11,7 +11,7 @@ fi
 # Fetch secrets from Secret Manager in CGP
 export MLFLOW_TRACKING_USERNAME="$(python3 /app/get_secret.py --project="${GCP_PROJECT}" --secret=mlflow_tracking_username)"
 export MLFLOW_TRACKING_PASSWORD="$(python3 /app/get_secret.py --project="${GCP_PROJECT}" --secret=mlflow_tracking_password)"
-export ARTIFACT_URL="$(python3 /app/get_secret.py --project="${GCP_PROJECT}" --secret=mlflow_artifact_url)"
+export ARTIFACT_URL="$(python3 /app/get_secret.py --project="${GCP_PROJECT}" --secret=mlflow_artifact_url1)"
 if [[ -z "${DATABASE_URL}" ]]; then # Allow overriding for local deployment
     export DATABASE_URL="$(python3 /app/get_secret.py --project="${GCP_PROJECT}" --secret=mlflow_database_url)"
 fi
